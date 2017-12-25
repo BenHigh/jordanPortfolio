aeafce89ef8f918f3752faedd8dafa3ab804c9bf
BenHigh <ben.high@austin.rr.com>
2017-12-25 15:20:28 -0600

    finalize for now

---
+++ server.js
@@ -0,0 +1,30 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+require("./routes/html-routes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
